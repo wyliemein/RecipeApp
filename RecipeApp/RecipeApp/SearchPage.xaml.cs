592c421bf3cd0ea218970eacc4c84208ca528686
zgao12 <zgao12@ucsc.edu>
2019-07-22 00:00:27 -0700

    updating

--- RecipeApp/RecipeApp/SearchPage.xaml.cs
+++ RecipeApp/RecipeApp/SearchPage.xaml.cs
@@ -54,7 +54,7 @@ namespace RecipeApp
             string type = "";
 
 
-            List<Recipes> person = await firebaseHelper.GetAllRecipes();
+            List<Recipes> person = await firebaseHelper.GetAllRecipes("Recipes");
 
             if (diet == null && deIn == null && calIn == null) {
                 await DisplayAlert("Fail", "Enter at least one Field", "OK");
