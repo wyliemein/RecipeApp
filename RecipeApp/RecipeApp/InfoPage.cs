980c4721944bc398c609af5b9501d7a8c941ee65
myyeung <myyeung@ucsc.edu>
2019-07-21 20:33:32 -0700

    fixed formatting

--- RecipeApp/RecipeApp/InfoPage.cs
+++ RecipeApp/RecipeApp/InfoPage.cs
@@ -12,9 +12,9 @@ namespace RecipeApp
             {
                 Children = {
                     new Label { Text = "Welcome to the Recipe App," + "to find recipes:" +
-                    "1. Press the Find Recipe button." +
-                    "2. Choose a diet type and/ or a desired ingredient" +
-                    "3. Cycle through all recipes that fit you requirements" +
+                    "1. Press the Find Recipe button" +
+                    "2. Choose a diet type and/or a desired ingredient" +
+                    "3. Cycle through all recipes that fit your requirements" +
                     "4. Save Recipes for later"}
                 }
             };
