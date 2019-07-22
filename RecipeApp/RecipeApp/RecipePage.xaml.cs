ad46887faf7dc08c061b9d1779785a1ab47547e3
gmein <gmein@ucsc.edu>
2019-07-22 00:25:59 -0700

    fixed next bug

--- RecipeApp/RecipeApp/RecipePage.xaml.cs
+++ RecipeApp/RecipeApp/RecipePage.xaml.cs
@@ -34,42 +34,44 @@ namespace RecipeApp
         {
             if (SearchPage.temp.Count != 0)
             {
+                int index = getRandomNumber(SearchPage.temp.Count);
+                this.index = index;
 
-
-                Random rnd = new Random();
-                int i = rnd.Next(0, SearchPage.temp.Count);
-                index = i;
-
-                SearchPage.ResName = SearchPage.temp[i].Name;
-                SearchPage.Link = SearchPage.temp[i].URL;
-                SearchPage.IngredientList = SearchPage.temp[i].Ingredient;
-                SearchPage.CalciumList = SearchPage.temp[i].Calcium;
-                SearchPage.CaloriesList = SearchPage.temp[i].Calories;
-                SearchPage.CholesterolList = SearchPage.temp[i].Cholesterol;
-                SearchPage.DietaryFiberList = SearchPage.temp[i].DietaryFiber;
-                SearchPage.FolateList = SearchPage.temp[i].Folate;
-                SearchPage.IronList = SearchPage.temp[i].Iron;
-                SearchPage.MagnesiumList = SearchPage.temp[i].Magnesium;
-                SearchPage.NiacinList = SearchPage.temp[i].Niacin;
-                SearchPage.PotassiumList = SearchPage.temp[i].Potassium;
-                SearchPage.ProteinList = SearchPage.temp[i].Protein;
-                SearchPage.SaturatedFatList = SearchPage.temp[i].SaturatedFat;
-                SearchPage.SodiumList = SearchPage.temp[i].Sodium;
-                SearchPage.SugarsList = SearchPage.temp[i].Sugars;
-                SearchPage.ThiaminList = SearchPage.temp[i].Thiamin;
-                SearchPage.TotalCarbohydratesList = SearchPage.temp[i].TotalCarbohydrates;
-                SearchPage.TotalFatList = SearchPage.temp[i].TotalFat;
-                SearchPage.VitaminAList = SearchPage.temp[i].VitaminA;
-                SearchPage.VitaminB6List = SearchPage.temp[i].VitaminB6;
-                SearchPage.VitaminCList = SearchPage.temp[i].VitaminC;
+                SearchPage.ResName = SearchPage.temp[this.index].Name;
+                SearchPage.Link = SearchPage.temp[index].URL;
+                SearchPage.IngredientList = SearchPage.temp[index].Ingredient;
+                SearchPage.CalciumList = SearchPage.temp[index].Calcium;
+                SearchPage.CaloriesList = SearchPage.temp[index].Calories;
+                SearchPage.CholesterolList = SearchPage.temp[index].Cholesterol;
+                SearchPage.DietaryFiberList = SearchPage.temp[index].DietaryFiber;
+                SearchPage.FolateList = SearchPage.temp[index].Folate;
+                SearchPage.IronList = SearchPage.temp[index].Iron;
+                SearchPage.MagnesiumList = SearchPage.temp[index].Magnesium;
+                SearchPage.NiacinList = SearchPage.temp[index].Niacin;
+                SearchPage.PotassiumList = SearchPage.temp[index].Potassium;
+                SearchPage.ProteinList = SearchPage.temp[index].Protein;
+                SearchPage.SaturatedFatList = SearchPage.temp[index].SaturatedFat;
+                SearchPage.SodiumList = SearchPage.temp[index].Sodium;
+                SearchPage.SugarsList = SearchPage.temp[index].Sugars;
+                SearchPage.ThiaminList = SearchPage.temp[index].Thiamin;
+                SearchPage.TotalCarbohydratesList = SearchPage.temp[index].TotalCarbohydrates;
+                SearchPage.TotalFatList = SearchPage.temp[index].TotalFat;
+                SearchPage.VitaminAList = SearchPage.temp[index].VitaminA;
+                SearchPage.VitaminB6List = SearchPage.temp[index].VitaminB6;
+                SearchPage.VitaminCList = SearchPage.temp[index].VitaminC;
                 char[] toTrim = { '[', '\'', ']' };
                 SearchPage.IngredientList = SearchPage.IngredientList.TrimStart(toTrim); //trims " [' " from start
                 SearchPage.IngredientList = SearchPage.IngredientList.TrimEnd(toTrim); //trims " ] " from end
                 SearchPage.IngredientList = SearchPage.IngredientList.Replace("', '", "\n");
-                SearchPage.temp.RemoveAt(i);
-                
-
-                await Navigation.PushAsync(new RecipePage());
+                if (SearchPage.temp.Count > 1)
+                {
+                    SearchPage.temp.RemoveAt(index);
+                    await Navigation.PushAsync(new RecipePage());
+                }
+                else
+                {
+                    await DisplayAlert("All Out", "There are no more recipes available", "OK");
+                }
             }
             else
             {
