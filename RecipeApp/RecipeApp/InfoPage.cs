34f2278a50de2f8adc38d84c80759d72a0b15b67
Michelle Yeung <myyeung@ucsc.edu>
2019-07-22 18:34:17 -0700

    updated info format

--- RecipeApp/RecipeApp/InfoPage.cs
+++ RecipeApp/RecipeApp/InfoPage.cs
@@ -13,8 +13,9 @@ namespace RecipeApp
                 Children = {
                     new Label { Text = "Welcome to the Recipe App! To find recipes:" +
                     "\n  1. Press the Find Recipe button" +
-                    "\n  2. Choose a diet type and/or a desired ingredient" +
-                    "\n  3. Cycle through all recipes that fit your requirements" +
+                    "\n  2. Specify your food preferences" +
+                    "\n  3. Cycle through all recipes that fit your" +
+                    "\n     requirements" +
                     "\n  4. Save Recipes for later"}
                 }
             };
